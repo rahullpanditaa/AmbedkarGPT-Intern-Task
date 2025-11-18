a7898f1d98baf13f27ae90ccf04a0613cb064169
Rahul Pandita <rpandita1999@outlook.com>
2025-11-18 02:05:06 +0000

    update build_vector_db method to split list[Document] based on splitter

--- retrieval/retrieval_logic.py
+++ retrieval/retrieval_logic.py
@@ -1,16 +1,31 @@
 from langchain_community.document_loaders import (
-    TextLoader,
+    TextLoader
 )
+from langchain_text_splitters import CharacterTextSplitter
+
 from .search_utils import SPEECH_TXT_PATH, DATA_DIR_PATH
 
 
 class Search:
-    def __init__(self):
-        ...
+    def __init__(self, model_name):
+        self.model = model_name
 
     def build_vector_db(self):
+        # load text
         DATA_DIR_PATH.mkdir(parents=True, exist_ok=True)
         loader = TextLoader(SPEECH_TXT_PATH)
         documents = loader.load()
-        return documents
+
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator=".", 
+            chunk_size=500,
+            chunk_overlap=50,
+            length_function=len,
+            is_separator_regex=False
+        )
+
+        docs_splits = text_splitter.split_documents(documents)
+        
+
 

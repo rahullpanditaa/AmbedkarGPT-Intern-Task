3d6f22a6ada0552dedc7ba60099a3fb7b6ba5aeb
Rahul Pandita <rpandita1999@outlook.com>
2025-11-18 08:45:46 +0000

    refactor rag chain runnables into a different module

---
+++ retrieval/rag_chain.py
@@ -0,0 +1,38 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from search_utils import combine_docs, PROMPT
+from .search import Search
+
+# invoke the llm
+# llm = OllamaLLM(model="mistral")
+
+# searcher = Search()
+# retriever = searcher.load_or_create_vector_db()
+
+
+# rag_inputs = {
+#     "context": retriever | document_combiner,
+#     "question": RunnablePassthrough()
+# }
+
+# rag_chain = rag_inputs | prompt | llm
+
+def create_rag_chain():
+    # invoke llm
+    llm = OllamaLLM(model="mistral")
+
+    # get retriever
+    searcher = Search()
+    retriever = searcher.load_or_create_vector_db()
+
+    # runnable that converts list[docs] into a string for llm
+    doc_combiner = RunnableLambda(combine_docs)
+
+    rag_inputs = {
+        "context": retriever | doc_combiner,
+        "question": RunnablePassthrough()
+    }
+
+    rag_chain = rag_inputs | PROMPT | llm
+    return rag_chain
+
--- retrieval/search_utils.py
+++ retrieval/search_utils.py
@@ -1,8 +1,21 @@
 from pathlib import Path
 from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
 
 
 DATA_DIR_PATH = Path(__file__).parent.parent.resolve() / "data"
 SPEECH_TXT_PATH = DATA_DIR_PATH / "speech.txt"
 
 CHROMA_DIR_PATH = Path(__file__).parent.resolve() / "chroma_vector_db"
+
+PROMPT = ChatPromptTemplate.from_template("""Answer the question based on the provided context,
+    
+    Question: {question}
+
+Context: 
+{context}
+
+Provide an answer that addresses the question:""")
+
+def combine_docs(docs: list[Document]):
+    return "\n\n".join(doc.page_content for doc in docs)

5d17e1f6ff4cfb88f5a9dd1b668d33b449faca70
Rahul Pandita <rpandita1999@outlook.com>
2025-11-18 02:40:40 +0000

    create embeddings vector for chunks

--- retrieval/retrieval_logic.py
+++ retrieval/retrieval_logic.py
@@ -1,14 +1,16 @@
-from langchain_community.document_loaders import (
-    TextLoader
-)
+from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
-
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from .search_utils import SPEECH_TXT_PATH, DATA_DIR_PATH
 
 
 class Search:
-    def __init__(self, model_name):
-        self.model = model_name
+    chunks_metadata: list[dict]
+
+    def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
+        self.model = HuggingFaceEmbeddings(model_name=model_name)
+        self.chunk_embeddings = None
+        self.chunks_metadata = None
 
     def build_vector_db(self):
         # load text
@@ -25,7 +27,13 @@ class Search:
             is_separator_regex=False
         )
 
-        docs_splits = text_splitter.split_documents(documents)
-        
+        docs_chunks = text_splitter.split_documents(documents)
 
+        # create chunk embeddings
+        chunk_texts = []
+        for chunk in docs_chunks:
+            self.chunks_metadata.append(chunk.metadata)
+            chunk_texts.append(chunk.page_content)
+        
+        self.chunk_embeddings = self.model.embed_documents(chunk_texts)
 

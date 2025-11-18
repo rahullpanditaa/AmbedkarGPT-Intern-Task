6b3b6105ae3a77df5b5089dea49d97aff74d5da2
Rahul Pandita <rpandita1999@outlook.com>
2025-11-18 06:09:11 +0000

    create a vector store; remove self.chunk_embeddings, created automatically

--- retrieval/retrieval_logic.py
+++ retrieval/retrieval_logic.py
@@ -1,16 +1,20 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from .search_utils import SPEECH_TXT_PATH, DATA_DIR_PATH
-
+from langchain_community.vectorstores import Chroma
+from .search_utils import (
+    SPEECH_TXT_PATH, 
+    DATA_DIR_PATH,
+    CHROMA_DIR_PATH
+)
 
 class Search:
     chunks_metadata: list[dict]
 
     def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
         self.model = HuggingFaceEmbeddings(model_name=model_name)
-        self.chunk_embeddings = None
-        self.chunks_metadata = None
+        # self.chunk_embeddings = None
+        self.chunks_metadata = []
 
     def build_vector_db(self):
         # load text
@@ -29,11 +33,13 @@ class Search:
 
         docs_chunks = text_splitter.split_documents(documents)
 
-        # create chunk embeddings
         chunk_texts = []
         for chunk in docs_chunks:
             self.chunks_metadata.append(chunk.metadata)
             chunk_texts.append(chunk.page_content)
-        
-        self.chunk_embeddings = self.model.embed_documents(chunk_texts)
 
+        # create vector store
+        CHROMA_DIR_PATH.mkdir(parents=True, exist_ok=True)
+        vector_store = Chroma.from_documents(documents=docs_chunks, embedding=self.model, 
+                                             collection_name="ambedkar-gpt",
+                                             persist_directory=str(CHROMA_DIR_PATH))
--- retrieval/search_utils.py
+++ retrieval/search_utils.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 DATA_DIR_PATH = Path(__file__).parent.parent.resolve() / "data"
 SPEECH_TXT_PATH = DATA_DIR_PATH / "speech.txt"
+
+CHROMA_DIR_PATH = Path(__file__).parent.resolve() / "chroma_vector_db"

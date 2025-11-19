0437038d43dc7ff1532eaa1add177336390860e9
Rahul Pandita <rpandita1999@outlook.com>
2025-11-19 08:42:14 +0000

    create function to calculate retrieval metrics for every test question result

--- lib/metrics/retrieval_metrics.py
+++ lib/metrics/retrieval_metrics.py
@@ -1,9 +1,6 @@
 from evaluation.evaluation import evaluate_config
 
-def calculate_retrieval_metrics():
-    # did any of the retrieved top k(k=5) sources match
-    # expected sources
-    # if even one match, return 1 else 0
+def calculate_retrieval_metrics(k: int=5):
     results: list[dict] = evaluate_config()
 
     results_with_metrics = []
@@ -14,11 +11,19 @@ def calculate_retrieval_metrics():
         hit_rate = calculate_hit_rate(expected_sources=expected_sources,
                                       retrieved_sources=retrieved_sources)
         
+        precision_score = calculate_precision_score(expected_sources=expected_sources,
+                                                    retrieved_sources=retrieved_sources)
+        mrr = calculate_mrr(expected_sources=expected_sources,
+                            retrieved_sources=retrieved_sources)
         new_result = result.copy()
-        new_result["hit_rate"] = hit_rate
+        new_result["hit_rate"] = hit_rate if result["answerable"] else None
+        new_result[f"precision_at_{k}"] = precision_score if result["answerable"] else None
+        new_result["mrr"] = mrr if result["answerable"] else None
+
         results_with_metrics.append(new_result)
 
 def calculate_hit_rate(expected_sources: list[str], retrieved_sources: list[str]) -> int:
+    # return 1 if even one match, else 0
     for src in retrieved_sources:
         if src in expected_sources:
             return 1
@@ -33,10 +38,9 @@ def calculate_precision_score(expected_sources: list[str], retrieved_sources: li
     return relevant_retrieved / len(retrieved_sources)
 
 def calculate_mrr(expected_sources: list[str], retrieved_sources: list[str]):
-    rank = 0
     for i, retrieved in enumerate(retrieved_sources, 1):
         if retrieved in expected_sources:
-            rank = i
-            return 1 / rank
+            return 1 / i
+            
     return 0
 

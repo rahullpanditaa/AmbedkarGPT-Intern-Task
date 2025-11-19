eae4ffa747e4b328c1c1a6756d0febd2126d63d4
Rahul Pandita <rpandita1999@outlook.com>
2025-11-19 02:28:25 +0000

    rag-pipeline works

--- lib/search.py
+++ lib/search.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.vectorstores import VectorStoreRetriever
 from .search_utils import (
     SPEECH_TXT_PATH, 
@@ -36,8 +36,8 @@ class SemanticSearch:
                                              embedding=self.model,
                                              collection_name="ambedkar-gpt",                                             
                                              persist_directory=str(CHROMA_DIR_PATH))
-        print("Building a ChromaDB...")
-        print("Created vector store!!")
+        print(" - Building a ChromaDB...")
+        print(" - Created vector store!!\n")
         # retriever
         vector_store_retriever = vector_store.as_retriever(search_type="similarity",
                                                            search_kwargs={"k": 5})
@@ -45,7 +45,7 @@ class SemanticSearch:
 
     def load_or_create_vector_db(self) -> VectorStoreRetriever:
         if CHROMA_DIR_PATH.exists() and (CHROMA_DIR_PATH / "chroma.sqlite3").exists():
-            print("Loading vector store from disk...")
+            print(" - Loading vector store from disk...\n")
             # load db
             vs = Chroma(collection_name="ambedkar-gpt", 
                         embedding_function=self.model, 

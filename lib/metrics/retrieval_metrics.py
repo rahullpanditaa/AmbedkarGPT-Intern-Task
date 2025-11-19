cc209aafc8bdc599a74f3e8786ab313378c87f25
Rahul Pandita <rpandita1999@outlook.com>
2025-11-19 08:06:12 +0000

    create functions to calculate Hit Rate@K and Precision@K for every result generated previously (per question)

--- lib/metrics/retrieval_metrics.py
+++ lib/metrics/retrieval_metrics.py
@@ -1,4 +1,36 @@
+from evaluation.evaluation import evaluate_config
 
+def calculate_retrieval_metrics():
+    # did any of the retrieved top k(k=5) sources match
+    # expected sources
+    # if even one match, return 1 else 0
+    results: list[dict] = evaluate_config()
 
-def hit_rate():
+    results_with_metrics = []
+    
+    for result in results:
+        expected_sources = result["expected_docs_txt_files"]
+        retrieved_sources = result["retrieved_docs_txt_files"]
+        hit_rate = calculate_hit_rate(expected_sources=expected_sources,
+                                      retrieved_sources=retrieved_sources)
+        
+        new_result = result.copy()
+        new_result["hit_rate"] = hit_rate
+        results_with_metrics.append(new_result)
+
+def calculate_hit_rate(expected_sources: list[str], retrieved_sources: list[str]) -> int:
+    for src in retrieved_sources:
+        if src in expected_sources:
+            return 1
+    return 0
+
+def calculate_precision_score(expected_sources: list[str], retrieved_sources: list[str]) -> float:
+    # relevant retrieved / total retrieved
+    relevant_retrieved = 0
+    for src in retrieved_sources:
+        if src in expected_sources:
+            relevant_retrieved += 1
+    return relevant_retrieved / len(retrieved_sources)
+
+def calculate_mrr(expected_sources: list[str], retrieved_sources: list[str]):
     ...

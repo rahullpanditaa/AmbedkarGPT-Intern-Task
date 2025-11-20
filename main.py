f46aba16ba4c843e9abc8226a99d053adb08adac
Rahul Pandita <rpandita1999@outlook.com>
2025-11-20 08:10:02 +0000

    update main.py to run eval, agg for one config at a time

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lib.evaluation.evaluation import complete_evaluation, aggregate_results
 
 def main():
     complete_evaluation(cfg_name="small")
-    # aggregate_results()
+    aggregate_results(cfg_name="small")
 
 
 if __name__ == "__main__":
